e2d7d7a2135e95d8149b9a1ee25266d4cd87ff9d
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-04 19:51:56 +0200

    style the forgot password page

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 import { useNavigate  } from 'react-router-dom';
 import './ForgotPassword.css';
 
 const ForgotPassword = () => {
     const navigate = useNavigate();
 
-    return (
-        <div class="bg-gradient-primary">
+    const handleClickSignUp = () => {
+        navigate('/signUp');
+    };
 
+    const handleClickLogIn = () => {
+        navigate('/logIn');
+    };
+
+    const handleClickHome = () => {
+        navigate('/');
+    };
+
+    return (
         <div class="container">
 
             <div class="row justify-content-center">
@@ -24,7 +33,7 @@ const ForgotPassword = () => {
                                     <div class="p-5">
                                         <div class="text-center">
                                             <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
-                                            <p class="mb-4">We get it, stuff happens. Just enter your email address below
+                                            <p class="mb-4 forgot-password-data-label">We get it, stuff happens. Just enter your email address below
                                                 and we'll send you a link to reset your password!</p>
                                         </div>
                                         <form class="user">
@@ -33,16 +42,16 @@ const ForgotPassword = () => {
                                                     id="exampleInputEmail" aria-describedby="emailHelp"
                                                     placeholder="Enter Email Address..."/>
                                             </div>
-                                            <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                            <a class="btn btn-primary btn-user btn-block" onClick={handleClickHome}>
                                                 Reset Password
                                             </a>
                                         </form>
                                         <hr/>
                                         <div class="text-center">
-                                            <a class="small" href="register.html">Create an Account!</a>
+                                            <a class="small cursor-pointer" onClick={handleClickSignUp}>Create an Account!</a>
                                         </div>
                                         <div class="text-center">
-                                            <a class="small" href="login.html">Already have an account? Login!</a>
+                                            <a class="small cursor-pointer" onClick={handleClickLogIn}>Already have an account? Login!</a>
                                         </div>
                                     </div>
                                 </div>
@@ -55,7 +64,6 @@ const ForgotPassword = () => {
             </div>
 
         </div>
-    </div>
     );
 };
 
