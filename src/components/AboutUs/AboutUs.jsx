714bb5c288512887c4e1f934b9f683d46ec3509b
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-04 15:21:41 +0200

    edit about us page

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './AboutUs.css';
+
+const AboutUs = () => {
+
+    return (
+        <div className='aboutUs-backgroundimg'>
+            <div className='aboutUs-details-container'>
+                <Card style={{width: '100%', background: 'rgba(192, 192, 192, 1)'}} >
+                    <Card.Body>
+                        <h4><b>Ort Braude - Client and Server Technologies Course 2023</b></h4>
+                        <h3>Website to manage Car Service Information</h3>
+                        Submited by:
+                        Yassmine Machour & Moran Levi
+                    </Card.Body>
+                </Card>
+                <Card style={{width: '100%', background: 'rgba(192, 192, 192, 1)'}} >
+                    <Card.Body>
+                    <h4><b>What we offer?</b></h4>
+                    Web application to manage car service information, such as: save, update and delete treatments date of cars that comes for treatment.
+                    <br/>The number of car and worker who perfom this treatment is saved.
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,11 +15,16 @@ const Home = () => {
         navigate('/signUp');
     };
 
+    const handleClickAboutUs = () => {
+        navigate('/aboutUs');
+    };
+    
     return (
         <div id='Home' className='backgroundimg'>
             <div className='container'>
                 <h1 className='text-center'>Welcome to Car Service Information</h1>
                 <div className='buttons-container'>
+                    <Button className='' onClick={handleClickAboutUs}>About Us</Button>
                     <Button className='' onClick={handleClickLogIn}>Log In</Button>
                     <Button className='' onClick={handleClickSignUp}>Sign Up</Button>
                 </div>
