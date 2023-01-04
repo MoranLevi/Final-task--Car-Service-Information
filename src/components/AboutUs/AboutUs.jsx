cd59a9948f5ed03c5773a8cf8ed812b68401e219
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-04 19:59:20 +0200

    style the about us page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -8,15 +8,15 @@ const AboutUs = () => {
     return (
         <div className='aboutUs-backgroundimg'>
             <div className='aboutUs-details-container'>
-                <Card style={{width: '100%', background: 'rgba(192, 192, 192, 1)'}} >
+                <Card style={{width: '100%', color: 'black', background: 'rgba(192, 192, 192, 1)'}} >
                     <Card.Body>
                         <h4><b>Ort Braude - Client and Server Technologies Course 2023</b></h4>
-                        <h3>Website to manage Car Service Information</h3>
+                        <h3 style= {{color: 'black'}}>Website to manage Car Service Information</h3>
                         Submited by:
                         Yassmine Machour & Moran Levi
                     </Card.Body>
                 </Card>
-                <Card style={{width: '100%', background: 'rgba(192, 192, 192, 1)'}} >
+                <Card style={{width: '100%', color: 'black', background: 'rgba(192, 192, 192, 1)'}} >
                     <Card.Body>
                     <h4><b>What we offer?</b></h4>
                     Web application to manage car service information, such as: save, update and delete treatments date of cars that comes for treatment.
