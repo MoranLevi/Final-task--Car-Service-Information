5a73fef2cb52792377fe3bfdfe676474b0d87d3a
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-09 23:18:19 +0200

    start validation in sign up

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useNavigate  } from 'react-router-dom';
+import { signUpSchema } from 'Validations/SignUpValidation';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SignUp.css';
 import '../../css/sb-admin-2.css';
@@ -16,6 +20,18 @@ const SignUp = () => {
         navigate('/logIn');
     };
     
+    const { register, handleSubmit, formState: { errors }} = useForm({
+        resolver: yupResolver(signUpSchema),
+        mode: "onChange"
+    });
+
+    const submitForm = (data) => {
+        //add fetch to send data to backend
+        console.log(data);
+        handleClickHome();
+    };
+    
+    console.error(errors);
     return (
         <div className="container">
 
@@ -28,34 +44,40 @@ const SignUp = () => {
                                 <div className="text-center">
                                     <h1 className="h4 text-gray-900 mb-4">Create an Account!</h1>
                                 </div>
-                                <form className="user">
+                                <form className="user" onSubmit={handleSubmit(submitForm)}>
                                     <div className="form-group row">
                                         <div className="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="text" className="form-control form-control-user" id="exampleFirstName"
-                                                placeholder="First Name"/>
+                                            <input type="text" className="form-control form-control-user" name="firstName"
+                                                placeholder="First Name" {...register('firstName')}/>
+                                            <p className='error-msg'>{errors.firstName ? errors.firstName?.message : ''}</p>
                                         </div>
                                         <div className="col-sm-6">
-                                            <input type="text" className="form-control form-control-user" id="exampleLastName"
-                                                placeholder="Last Name"/>
+                                            <input type="text" className="form-control form-control-user" name="lastName"
+                                                placeholder="Last Name" {...register('lastName')}/>
+                                            <p className='error-msg'>{errors?.lastName ? errors.lastName?.message : ''}</p>
                                         </div>
                                     </div>
                                     <div className="form-group">
-                                        <input type="email" className="form-control form-control-user" id="exampleInputEmail"
-                                            placeholder="Email Address"/>
+                                        <input type="email" className="form-control form-control-user" name="email"
+                                            placeholder="Email Address" {...register('email')}/>
+                                        <p className='error-msg'>{errors.email ? errors.email?.message : ''}</p>
                                     </div>
                                     <div className="form-group row">
                                         <div className="col-sm-6 mb-3 mb-sm-0">
                                             <input type="password" className="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password"/>
+                                                name="password" placeholder="Password" {...register('password')}/>
+                                            <p className='error-msg'>{errors?.password ? errors.password?.message : ' '}</p>
                                         </div>
                                         <div className="col-sm-6">
                                             <input type="password" className="form-control form-control-user"
-                                                id="exampleRepeatPassword" placeholder="Repeat Password"/>
+                                                name="repeatPassword" placeholder="Repeat Password" {...register('repeatPassword')}/>
+                                            <p className='error-msg'>{errors.repeatPassword ? errors.repeatPassword?.message : ' '}</p>
                                         </div>
                                     </div>
-                                    <a className="btn btn-primary btn-user btn-block" onClick={handleClickHome}>
+                                    {/* <a className="btn btn-primary btn-user btn-block">
                                         Register Account
-                                    </a>
+                                    </a> */}
+                                    <input type="submit" className="btn btn-primary btn-user btn-block" value={'Register Account'}></input>
                                 </form>
                                 <hr/>
                                 <div className="text-center">
