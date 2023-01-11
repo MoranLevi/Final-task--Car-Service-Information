baf952d5657fd1a03ec6bd9c8c527cb1c59ead80
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-11 17:10:52 +0200

    change from characters to chars

--- src/Validations/SignUpValidation.js
+++ src/Validations/SignUpValidation.js
@@ -10,6 +10,6 @@ export const signUpSchema = yup.object().shape({
     firstName: yup.string().required('First name is required').test('is-letters-only', 'First name can only contain letters', checkIfOnlyLetters),
     lastName: yup.string().required('Last name is required').test('is-letters-only', 'Last name can only contain letters', checkIfOnlyLetters),
     email: yup.string().email('Invalid email').required('Email is required'),
-    password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+    password: yup.string().min(6, 'Password must be at least 6 chars').required('Password is required'),
     repeatPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Confirm password is required')
 });
