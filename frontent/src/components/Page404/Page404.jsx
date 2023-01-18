fbd501821b1901a91e855c255caf761b6fb387c5
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-18 23:51:58 +0200

    update page 404

--- frontent/src/components/Page404/Page404.jsx
+++ frontent/src/components/Page404/Page404.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import lego-man-image from "../../images/404-lego-man-image.png";
-import imageLego from '../../images/404-lego-man-image.png';
+import image from '../../images/error-404-image.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Page404.css';
 
@@ -11,10 +10,9 @@ const Page404 = () => {
             <div className="d-flex align-items-center justify-content-center vh-100">
                 <div className="text-center row">
                     <div className=" col-md-6">
-                        <img src={imageLego} alt='imageLego' className="img-fluid"/>
+                        <img src={image} alt='imageLego' className="img-fluid"/>
                     </div>
                     <div className=" col-md-6 mt-5">
-                        <h1 className="display-1 fw-bold">404</h1>
                         <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
                         <p className="lead">
                             The page you’re looking for doesn’t exist.
