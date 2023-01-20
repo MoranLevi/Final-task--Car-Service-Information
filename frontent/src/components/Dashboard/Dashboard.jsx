39a172749b3e63e7db3f5b1f22cf4a01bebb4980
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-20 15:56:04 +0200

    add hover to buttons dashboard

--- frontent/src/components/Dashboard/Dashboard.jsx
+++ frontent/src/components/Dashboard/Dashboard.jsx
@@ -78,7 +78,6 @@ const Dashboard = () => {
                             <img src={imageEdit} alt="image-button" style={{ width: '30px', height: '30px' }}/>
                         </button>
                         <button onClick={(e) => onClickDelete(row.row)} className='button-image'>
-                        {/* <button onClick={() => console.log('Button clicked for row', row.row.original.treatmentNumber)} className='button-image'> */}
                             <img src={imageDelete} alt="image-button" style={{ width: '30px', height: '30px' }}/>
                         </button>
                     </div>     
