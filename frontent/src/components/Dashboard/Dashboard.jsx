25d58820b0a5697b530ba685a9142ca2f595c4e2
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-21 01:52:51 +0200

    add container

--- frontent/src/components/Dashboard/Dashboard.jsx
+++ frontent/src/components/Dashboard/Dashboard.jsx
@@ -115,127 +115,130 @@ const Dashboard = () => {
     );
 
     return (
-        <div id="page-top">
+        <div className='container'>
+            <div id="page-top">
 
-            {/* Page Wrapper */}
-            <div id="wrapper">
+                {/* Page Wrapper */}
+                <div id="wrapper">
 
-                {/* Content Wrapper */}
-                <div id="content-wrapper" className="d-flex flex-column">
+                    {/* Content Wrapper */}
+                    <div id="content-wrapper" className="d-flex flex-column">
 
-                    {/* Main Content */}
-                    <div id="content">
+                        {/* Main Content */}
+                        <div id="content">
 
-                        {/* Topbar */}
-                        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                            {/* Topbar */}
+                            <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                                
+                                <a href="#" className="nav-link link-log-out" onClick={handleOnClickLogOut}>Log Out</a>
+                                {/* Topbar Navbar */}
+                                <ul className="navbar-nav ml-auto">
+                                    
 
-                            {/* Topbar Navbar */}
-                            <ul className="navbar-nav ml-auto">
-                                {/* <a href="#" className="nav-link link-log-out" onClick={handleOnClickLogOut}>Log Out</a> */}
+                                    <div className="topbar-divider d-none d-sm-block"></div>
 
-                                <div className="topbar-divider d-none d-sm-block"></div>
-
-                                {/* Nav Item - User Information */}
-                                <li className="nav-item dropdown no-arrow">
-                                    <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <span className="mr-2 d-none d-lg-inline text-gray-600 big">{`${user?.firstName} ${user?.lastName}`}</span>
-                                        <img className="img-profile rounded-circle" src={imageProfile} style={{ width: '45px', height: '40px' }}/>
-                                    </a>
-                                    {/* Dropdown - User Information */}
-                                    <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                        aria-labelledby="userDropdown">
-                                        <a className="dropdown-item" href="#">
-                                            <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Profile
+                                    {/* Nav Item - User Information */}
+                                    <li className="nav-item dropdown no-arrow">
+                                        <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <span className="mr-2 d-none d-lg-inline text-gray-600 big">{`${user?.firstName} ${user?.lastName}`}</span>
+                                            <img className="img-profile rounded-circle" src={imageProfile} style={{ width: '45px', height: '40px' }}/>
                                         </a>
-                                        <a className="dropdown-item" href="#">
-                                            <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Settings
-                                        </a>
-                                        <a className="dropdown-item" href="#">
-                                            <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Activity Log
-                                        </a>
-                                        <div className="dropdown-divider"></div>
-                                        <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                            <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Logout
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </nav>
-                        {/* End of Topbar */}
+                                        {/* Dropdown - User Information */}
+                                        <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                            aria-labelledby="userDropdown">
+                                            <a className="dropdown-item" href="#">
+                                                <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                                Profile
+                                            </a>
+                                            <a className="dropdown-item" href="#">
+                                                <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                                Settings
+                                            </a>
+                                            <a className="dropdown-item" href="#">
+                                                <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                                Activity Log
+                                            </a>
+                                            <div className="dropdown-divider"></div>
+                                            <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                                <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                                Logout
+                                            </a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </nav>
+                            {/* End of Topbar */}
 
-                        {/* Begin Page Content */}
-                        <div className="container-fluid">
+                            {/* Begin Page Content */}
+                            <div className="container-fluid">
 
-                            {/* Page Heading */}
-                            <h1 className="h3 mb-2 text-gray-800">Car Service Table</h1>
-                            {/* <p className="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-                                For more information about DataTables, please visit the <a target="_blank"
-                                    href="https://datatables.net">official DataTables documentation</a>.</p> */}
-                            <button className="mb-4 btn btn-primary" onClick={handleClickAddNewCarService}>Add New Car Service</button>
-                            {/* DataTales Example */}
-                            <div className="card shadow mb-4">
-                                {/* <div className="card-header py-3">
-                                    <h6 className="m-0 font-weight-bold text-primary">DataTables Example</h6>
-                                </div> */}
-                                <div className="card-body">
-                                    <div className="table-responsive">
-                                        <table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                            <ReactTable columns={tableColumns} data={carsTableData} />
-                                        </table>
+                                {/* Page Heading */}
+                                <h1 className="h3 mb-2 text-gray-800">Car Service Table</h1>
+                                {/* <p className="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+                                    For more information about DataTables, please visit the <a target="_blank"
+                                        href="https://datatables.net">official DataTables documentation</a>.</p> */}
+                                <button className="mb-4 btn btn-primary" onClick={handleClickAddNewCarService}>Add New Car Service</button>
+                                {/* DataTales Example */}
+                                <div className="card shadow mb-4">
+                                    {/* <div className="card-header py-3">
+                                        <h6 className="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                                    </div> */}
+                                    <div className="card-body">
+                                        <div className="table-responsive">
+                                            <table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                                <ReactTable columns={tableColumns} data={carsTableData} />
+                                            </table>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
+                            </div>
+                            {/* /.container-fluid */}
                         </div>
-                        {/* /.container-fluid */}
+                        {/* End of Main Content */}
+
+                        {/* Footer */}
+                        {/* <footer className="sticky-footer bg-white">
+                            <div className="container my-auto">
+                                <div className="copyright text-center my-auto">
+                                    <span>Copyright &copy; Your Website 2020</span>
+                                </div>
+                            </div>
+                        </footer> */}
+                        {/* End of Footer */}
+
                     </div>
-                    {/* End of Main Content */}
-
-                    {/* Footer */}
-                    {/* <footer className="sticky-footer bg-white">
-                        <div className="container my-auto">
-                            <div className="copyright text-center my-auto">
-                                <span>Copyright &copy; Your Website 2020</span>
-                            </div>
-                        </div>
-                    </footer> */}
-                    {/* End of Footer */}
+                    {/* End of Content Wrapper */}
 
                 </div>
-                {/* End of Content Wrapper */}
+                {/* End of Page Wrapper */}
 
-            </div>
-            {/* End of Page Wrapper */}
+                {/* Scroll to Top Button*/}
+                <a className="scroll-to-top rounded" href="#page-top">
+                    <i className="fas fa-angle-up"></i>
+                </a>
 
-            {/* Scroll to Top Button*/}
-            <a className="scroll-to-top rounded" href="#page-top">
-                <i className="fas fa-angle-up"></i>
-            </a>
-
-            {/* Logout Modal*/}
-            <div className="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                            <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                        <div className="modal-footer">
-                            <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                            <a className="btn btn-primary" href="login.html">Logout</a>
+                {/* Logout Modal*/}
+                <div className="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                            <div className="modal-footer">
+                                <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                <a className="btn btn-primary" href="login.html">Logout</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+        </div>
     </div>
     );
 };
