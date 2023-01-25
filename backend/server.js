ceb5e55af2493c04b4848cdf10b62da2a1143458
YassmineMachour <114674038+YassmineMachour172@users.noreply.github.com>
2023-01-25 21:41:32 +0200

    a

--- backend/server.js
+++ backend/server.js
@@ -316,7 +316,8 @@ app.post('/reCaptchaValidation', async (req, res) => {
     }else{ // if the request is invalid
         console.log('reCAPTCHA verification failed');
         res.status(400)
-        res.send("ReCAPTCHA verification failed", token)
+        // res.send("ReCAPTCHA verification failed", token)
+        res.send(token)
         return
     }
 })
