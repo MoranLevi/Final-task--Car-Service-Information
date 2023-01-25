ccbb1131008d9a832330a4eb9ea8b03f15430cad
YassmineMachour <114674038+YassmineMachour172@users.noreply.github.com>
2023-01-25 20:58:03 +0200

    try

--- frontent/src/components/LogIn/LogIn.jsx
+++ frontent/src/components/LogIn/LogIn.jsx
@@ -181,7 +181,7 @@ const LogIn = () => {
                                             </div>
                                             <center className='margin-bottom-ReCAPTCHA'><ReCAPTCHA /* ReCAPTCHA component */
 
-                                                sitekey="6LeLhSgkAAAAADVvrCx-SHXm9WABZuR-lxfszuX0"
+                                                sitekey="6LclkigkAAAAALQcdvez43N02D0exRaiZaKPLSkn"
                                                 ref={captchaRef}
                                             /></center>
                                             <input type="submit" className="btn btn-primary btn-user btn-block" value={'Login'}></input>
--- frontent/src/components/SignUp/SignUp.jsx
+++ frontent/src/components/SignUp/SignUp.jsx
@@ -151,7 +151,7 @@ const SignUp = () => {
                                         </div>
                                     </div>
                                     <center className='margin-bottom-ReCAPTCHA'><ReCAPTCHA /* display the reCAPTCHA */
-                                        sitekey="6LeLhSgkAAAAADVvrCx-SHXm9WABZuR-lxfszuX0"
+                                        sitekey="6LclkigkAAAAALQcdvez43N02D0exRaiZaKPLSkn"
                                         ref={captchaRef}/>
                                     </center>
                                     <input type="submit" className="btn btn-primary btn-user btn-block" value={'Register Account'}></input> 
