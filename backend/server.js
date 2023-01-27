fc831a63a6dd4f68367a44a6c07dd885440efe4a
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-27 12:18:32 +0200

    Revert "l"
    This reverts commit 52acd19d677a6cd5c281c8a15ccb1d54f569c667.

--- backend/server.js
+++ backend/server.js
@@ -296,7 +296,7 @@ app.post('/reCaptchaValidation', async (req, res) => {
     const token = req.body.token;
 
     await axios.post(
-        `https://www.google.com/recaptcha/api/siteverify?secret=6LcgkygkAAAAAARqQkloeTrCf-3n_gySEIMg8emM&response=${token}`
+        `https://www.google.com/recaptcha/api/siteverify?secret=6LclkigkAAAAACwtJ5MQjglC51xi_WI7di0ztVYP&response=${token}`
     );
 
     if (res.status(200)) { // if the request is valid
